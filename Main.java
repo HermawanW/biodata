59c9711f74d9ebd170cd222abd31f5ffebd24067
HermawanW <69895640+HermawanW@users.noreply.github.com>
2020-08-19 18:43:09 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Nama : Hermawan Wicaksono");
+        System.out.println("Jenis kelamin : Laki-laki");
+        System.out.println("Alamat : JL. KHA Dahlan Pasuruan");
+        System.out.println("Jurusan : RPL");
+    }
+    
+}
